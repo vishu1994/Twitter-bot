d2df8644008bb470d42ea90a42b8104e9e71d2f4
sanjeet <sanjeetkr987152@gmail.com>
2022-09-16 03:19:07 +0530

    improve its main() to show data correctly

--- follow_followers_bot/not_followed_followers.py
+++ follow_followers_bot/not_followed_followers.py
@@ -1,15 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
 import followers_lookup
 import following_look_up
 def main():
     not_following = list(set(followers_lookup.main())-set(following_look_up.main()))
     return not_following
+    
 if __name__=="__main__":
     c = main()
-    print(c)
-
-
-# In[ ]:
-
-
-
+    if c == []:
+        print("Everything is up to date")
+    else:
+        print(c)
 
